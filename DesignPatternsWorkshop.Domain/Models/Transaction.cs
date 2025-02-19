f3a5df5f3a25aea9c9166f26366962694f1c05ea
Alphonso Varela <alphonso.varela@islington.gov.uk>
2025-02-19 14:51:38 +0000

    feat: create Transaction and Product domain models

---
+++ DesignPatternsWorkshop.Domain/Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPatternsWorkshop.Domain.Models;
+
+public class Transaction
+{
+    public int Id { get; set; }
+    public List<Product> Products { get; set; }
+    public decimal GetTotal()
+    {
+        decimal total = Products.Sum(p => p.Price * p.Quantity);
+        return total;
+    }
+}
