70e3b92c16c2643ce76f86a1b392017e870e0006
Alphonso Varela <alphonso.varela@islington.gov.uk>
2025-02-20 14:18:15 +0000

    fix: register Purchase Service as singleton instead of scoped

--- DesignPatternsWorkshop.Infrastructure/Program.cs
+++ DesignPatternsWorkshop.Infrastructure/Program.cs
@@ -1,3 +1,5 @@
+using DesignPatternsWorkshop.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,18 +7,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<PurchaseService>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("WebDesignPatternsWorkshopPolicy", policy =>
+    {
+        policy
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.))
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
+app.UseCors("WebDesignPatternsWorkshopPolicy");
 app.UseHttpsRedirection();
 app.MapControllerRoute("default", "/[Controller]/[Action]/");
 
