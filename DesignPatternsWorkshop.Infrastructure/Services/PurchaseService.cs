cfbffcbce0d7460594e83adf0ded703f60d6b4ae
Alphonso Varela <alphonso.varela@islington.gov.uk>
2025-02-20 12:49:26 +0000

    feat: define service for purchase operations

---
+++ DesignPatternsWorkshop.Infrastructure/Services/PurchaseService.cs
@@ -0,0 +1,59 @@
+﻿using DesignPatternsWorkshop.Application.Commands;
+using DesignPatternsWorkshop.Application.Strategies;
+using DesignPatternsWorkshop.Domain.Models;
+using DesignPatternsWorkshop.Infrastructure.Commands;
+
+namespace DesignPatternsWorkshop.Infrastructure.Services;
+
+public class PurchaseService
+{
+    #region properties
+    private readonly Purchase _purchase;
+    private readonly PurchaseInvoker _invoker;
+    #endregion
+
+    #region constructor
+    public PurchaseService()
+    {
+        _purchase = new Purchase();
+        _invoker = new PurchaseInvoker();
+    }
+    #endregion
+
+    #region methods
+    public Purchase FinalisePurchase() => _purchase;
+
+    public void AddProduct(Product product)
+    {
+        var command = new AddProductCommand(_purchase, product);
+        _invoker.ExecuteCommand(command);
+    }
+
+    public void RemoveProduct(Product product)
+    {
+        var command = new RemoveProductCommand(_purchase, product);
+        _invoker.ExecuteCommand(command);
+    }
+
+    public void UndoLastAction()
+    {
+        _invoker.Undo();
+    }
+
+    public void RedoLastAction()
+    {
+        _invoker.Redo();
+    }
+
+    public void ApplyDiscount(IDiscountStrategy discount)
+    {
+        _purchase.SetDiscountStrategy(discount);
+    }
+
+    public double CalculateTotal()
+    {
+        var total = _purchase.GetTotal();
+        return total;
+    }
+    #endregion
+}
