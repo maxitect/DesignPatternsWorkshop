a00bff1a1ab8743f1091c6eaf39b54e38d0b3c55
Alphonso Varela <alphonso.varela@islington.gov.uk>
2025-02-20 14:01:35 +0000

    docs: document public Purchase methods

--- DesignPatternsWorkshop.Domain/Models/Purchase.cs
+++ DesignPatternsWorkshop.Domain/Models/Purchase.cs
@@ -19,10 +19,18 @@ public class Purchase
     #endregion
 
     #region methods
+    /// <summary>
+    /// Applies the given Discount Strategy to the Purchase total.
+    /// </summary>
+    /// <param name="discount"></param>
     public void SetDiscountStrategy(IDiscountStrategy discount)
     {
         _discount = discount;
     }
+    /// <summary>
+    /// Returns the sum of all Product prices with any active Discount Strategy applied to the total.
+    /// </summary>
+    /// <returns></returns>
     public double GetTotal()
     {
         double total = Products.Sum(p => p.Price * p.Quantity);
