e63b2d49b55b03578304368e631d48052fac2806
maxitect <maxime.downe@gmail.com>
2025-04-10 15:46:05 +0100

    feat: implement undo/redo with key bindings challenge 3

--- DesignPatternsWorkshop.Presentation/wwwroot/js/signalRConnection.js
+++ DesignPatternsWorkshop.Presentation/wwwroot/js/signalRConnection.js
@@ -1,34 +1,46 @@
-import addProduct from "/js/addProduct.js"
-import removeProduct from "/js/removeProduct.js"
-import undoLast from "/js/undoLast.js"
-import redoLast from "/js/redoLast.js"
-import addDiscount from "/js/addDiscount.js"
+import addProduct from "/js/addProduct.js";
+import removeProduct from "/js/removeProduct.js";
+import undoLast from "/js/undoLast.js";
+import redoLast from "/js/redoLast.js";
+import addDiscount from "/js/addDiscount.js";
 
 export const connection = new signalR.HubConnectionBuilder()
   .withUrl("http://localhost:5130/purchase-hub")
   .withAutomaticReconnect()
-  .build()
+  .build();
 
-connection.start().catch(error => console.error(error))
+connection.start().catch((error) => console.error(error));
 
 connection.on("UpdatePurchase", function () {
   fetch("Home/GetProductList")
-    .then(response => response.text())
+    .then((response) => response.text())
     .then(
-      html => (document.getElementById("purchaseContainer").innerHTML = html)
+      (html) => (document.getElementById("purchaseContainer").innerHTML = html)
     )
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
 
   fetch("Home/GetTotal")
-    .then(response => response.text())
+    .then((response) => response.text())
     .then(
-      html => (document.getElementById("purchaseTotalDisplay").innerHTML = html)
+      (html) =>
+        (document.getElementById("purchaseTotalDisplay").innerHTML = html)
     )
-    .catch(error => console.error(error))
-})
+    .catch((error) => console.error(error));
+});
 
-window.addProduct = addProduct
-window.removeProduct = removeProduct
-window.undoLast = undoLast
-window.redoLast = redoLast
-window.addDiscount = addDiscount
+window.addProduct = addProduct;
+window.removeProduct = removeProduct;
+window.undoLast = undoLast;
+window.redoLast = redoLast;
+window.addDiscount = addDiscount;
+
+window.addEventListener("keydown", function (event) {
+  if (event.ctrlKey && event.key === "z") {
+    event.preventDefault();
+    return undoLast;
+  }
+  if (event.ctrlKey && event.key === "y") {
+    event.preventDefault();
+    return redoLast;
+  }
+});
